d4249d05407530533dfe23954b986eed8d6b61cf
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-22 08:39:25 -0600

    Uploading Template

---
+++ main.c
@@ -0,0 +1,14 @@
+/** main.c
+ * =============================================================
+ * Name: Your Name
+ * Section:  Your Section
+ * Project:  FILL IN
+ * Purpose:  FILL IN
+ * Documentation Statement:  FILL IN FOR FINAL TURN IN
+ * ============================================================= */
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}

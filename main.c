9e9c5c8ac85278e7806d266a349077f19681884f
troywein <45960380+troywein@users.noreply.github.com>
2023-12-05 11:20:57 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,53 @@
  * Project:  FILL IN
  * Purpose:  FILL IN
  * Documentation Statement:  FILL IN FOR FINAL TURN IN
+ * =============================================================
+ * 
+ * REQUIREMENTS NOT MET (REMOVED): <list any requirements from your design document that you failed to meet> or N/A
+ * 
+ * REQUIREMENTS MODIFIED:  <list any requirements from your design document that you changed> or N/A
+ *
+ * REQUIREMENTS ADDED: <list any requirements you added since your design document> or N/A
+ * 
+ * REQUIRED PROGRAMMING SKILLS (must have 5 of 6):
+ * -- Delete if skill doesn't apply
+ *    1) Pointer use with dynamic memory (malloc and free)
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    2) Pointers as a key component of a data structure
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    3) File I/O
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    4) Structs
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    5) String manipulation
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    6) Processing of array data
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ * REQUIRED ADVANCED PROGRAMMING SKILLS (1 of the below):
+ * -- Delete if skill doesn't apply
+ *    1) Recursion
+ *    Where in your code (file/line #'s) do you meet this req:
+ *  
+ *    2) 2D Dynamic Memory
+ *    Where in your code (file/line #'s) do you meet this req:
+ *  
+ *    3) Linked List (or some other advanced data structure)
+ *    Where in your code (file/line #'s) do you meet this req:
+ *  
+ *    4) A GUI (xwindows for Ubuntu)
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ *    5) Some other advanced skill (approve with your instructor)
+ *    Where in your code (file/line #'s) do you meet this req:
+ * 
+ * COMPILATION AND RUNNING INSTRUCTIONS: <list here>
+ * 
  * ============================================================= */
 
 #include <stdio.h>
